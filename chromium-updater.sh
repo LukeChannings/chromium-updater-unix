5afd6dabb1763445abe1fe7624b0dd8f8f92e3e8
Luke Channings <thefuzzball2005@gmail.com>
2011-08-15 23:58:06 +0100

    Added basic check for Chromium's existence.

--- chromium-updater.sh
+++ chromium-updater.sh
@@ -14,16 +14,29 @@ function system
 		Darwin)
 			OS="Mac"
 			ZIPNAME="chrome-mac"
+			INSTALLPATH="/Applications"
+			INSTALLNAME="Chromium.app"
 		;;
 		Linux)
 			OS="Linux"
 			ZIPNAME="chrome-linux"
+			INSTALLPATH="/opt/"
+			INSTALLNAME="chromium"
 		;;
 	esac
 }
 
 # Function to get the installed and current versions of Chromium.
-# function get_info {}
+function get_info {
 
-system
-echo $ZIPNAME.zip
+	# Call System
+	system
+
+	# Check if Chromium is installed.
+	if [ $INSTALLPATH/$INSTALLNAME ]; then
+		echo "Chromium exists."
+	fi
+
+}
+
+get_info
