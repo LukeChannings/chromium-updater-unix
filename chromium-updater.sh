5f2599977f5f2f42ed10a406cc095f9ce16f5926
Luke Channings <thefuzzball2005@gmail.com>
2011-08-15 23:19:21 +0100

    Added OS checking.

--- chromium-updater.sh
+++ chromium-updater.sh
@@ -1,1 +1,29 @@
 #!/bin/sh
+#
+# Chromium Updater
+# - For Mac and Linux
+# Copyright 2011 Luke Channings
+#
+
+# Function to set operating variables.
+function system
+{
+	# Check if the system is OS X or Linux.
+	UNAME=`uname`
+	case $UNAME in
+		Darwin)
+			OS="Mac"
+			ZIPNAME="chrome-mac"
+		;;
+		Linux)
+			OS="Linux"
+			ZIPNAME="chrome-linux"
+		;;
+	esac
+}
+
+# Function to get the installed and current versions of Chromium.
+# function get_info {}
+
+system
+echo $ZIPNAME.zip
