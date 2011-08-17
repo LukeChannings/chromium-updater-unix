71d2922e36adfc1c444be228bced525ba3fe4ace
Luke Channings <thefuzzball2005@gmail.com>
2011-08-17 17:50:36 +0100

    Remove 64-bit option in preparation for a cleaner approach.

--- chromium-updater.sh
+++ chromium-updater.sh
@@ -7,42 +7,42 @@
 
 # Function to set operating variables.
 function system {
-	# Check if the system is OS X or Linux.
-	UNAME=`uname`
-	case $UNAME in
-		Darwin)
-			OS="Mac"
-			ZIPNAME="chrome-mac"
-			INSTALLPATH="/Applications"
-			INSTALLBASE="Chromium.app"
-			INSTALLNAME="/Contents/MacOS/Chromium"
-			DM="curl" #Download Manager.
-			DMDDOPTS="-O" # File download option.
-			DMSOPTS="-s" # Streaming Options.
-            		# Check not Tiger.
-            		TIGERCHECK=`system_profiler SPSoftwareDataType | grep 'System Version' | grep "10.4"`
-           		 if [ ! -z "$TIGERCHECK" ]; then
-           		     echo "Chromium does not support OS X Tiger. Please upgrade OS X Leopard at least."
-           		     exit
-           		 fi
-		;;
-		Linux)
-			OS="Linux"
-			ZIPNAME="chrome-linux"
-			INSTALLPATH="/opt"
-			INSTALLBASE="chromium"
-			INSTALLNAME="chrome"
-			DM="wget" # Download Manager
-			DMSOPTS="-qO-" # Streaming Options
-		;;
-		\?)
-			echo "This system is not supported."
-			exit
-		;;
-	esac
+# Check if the system is OS X or Linux.
+UNAME=`uname`
+case $UNAME in
+Darwin)
+OS="Mac"
+ZIPNAME="chrome-mac"
+INSTALLPATH="/Applications"
+INSTALLBASE="Chromium.app"
+INSTALLNAME="/Contents/MacOS/Chromium"
+DM="curl" #Download Manager.
+DMDDOPTS="-O" # File download option.
+DMSOPTS="-s" # Streaming Options.
+# Check not Tiger.
+TIGERCHECK=`system_profiler SPSoftwareDataType | grep 'System Version' | grep "10.4"`
+if [ ! -z "$TIGERCHECK" ]; then
+echo "Chromium does not support OS X Tiger. Please upgrade OS X Leopard at least."
+exit
+fi
+;;
+Linux)
+OS="Linux"
+ZIPNAME="chrome-linux"
+INSTALLPATH="/opt"
+INSTALLBASE="chromium"
+INSTALLNAME="chrome"
+DM="wget" # Download Manager
+DMDDOPTS="" # File download option.
+DMSOPTS="-qO-" # Streaming Options
+;;
+\?)
+echo "This system is not supported."
+exit
+;;
+esac
 
-	INSTALL64BIT=false
-	SYSTEMCALLED=true
+SYSTEMCALLED=true
 
 }
 
@@ -50,20 +50,20 @@ function system {
 # $1 - SVN Revision Number.
 getVersion(){
 
-	# Make sure system was called.
-	if [ -z "$SYSTEMCALLED" ]; then
-		system
-	fi
+# Make sure system was called.
+if [ -z "$SYSTEMCALLED" ]; then
+system
+fi
 
-	# Make sure there is a revision number to work with.
-	if [ -z "$1" ]; then
-		echo "getVersion() was not passed a revision number."
-		exit
-	fi
+# Make sure there is a revision number to work with.
+if [ -z "$1" ]; then
+echo "getVersion() was not passed a revision number."
+exit
+fi
 
-	# Find the Version number of the revision.
-	RETURNEDVERSION=`$DM $DMSOPTS http://src.chromium.org/viewvc/chrome/trunk/src/chrome/VERSION?revision=$1 | \
-	sed -e 's/MAJOR=//' -e 's/MINOR=/./' -e 's/BUILD=/./' -e 's/PATCH=/./' | tr -d '\n'`
+# Find the Version number of the revision.
+RETURNEDVERSION=`$DM $DMSOPTS http://src.chromium.org/viewvc/chrome/trunk/src/chrome/VERSION?revision=$1 | \
+sed -e 's/MAJOR=//' -e 's/MINOR=/./' -e 's/BUILD=/./' -e 's/PATCH=/./' | tr -d '\n'`
 
 }
 
@@ -71,43 +71,43 @@ getVersion(){
 # $1: True - Disable current revision lookup.
 get_info() {
 
-	# Call System
-	system
+# Call System
+system
 
-	# Message...
-	printf "Gathering info...\t\t\t"
+# Message...
+printf "Gathering info...\t\t\t"
 
-	# Check Revision lookup parameter.
-	if [ -z "$1" ]; then
-		LOOKUPLATESTREVISION=true
-	else
-		LOOKUPLATESTREVISION=false
-	fi
+# Check Revision lookup parameter.
+if [ -z "$1" ]; then
+LOOKUPLATESTREVISION=true
+else
+LOOKUPLATESTREVISION=false
+fi
 
-	# Get information on the installed Chromium version.
-	if [ -f $INSTALLPATH/$INSTALLBASE/$INSTALLNAME ]; then
-		# Find version.
-		INSTALLEDVERSION=`$INSTALLPATH/$INSTALLBASE/$INSTALLNAME --version | sed "s/Chromium//" | sed "s/ //g"`
-		# Find SVN Revision. (Only possible on OS X sadly.)
-		if [ $OS == "Mac" ]; then
-			INSTALLEDREV=`cat /Applications/Chromium.app/Contents/Info.plist | grep -A 1 SVNRevision | grep -o "[[:digit:]]\+"`
-		fi
-		# Set installed variable for other functions.
-		INSTALLED=true
-	else
-		INSTALLED=false
-	fi
-	
-	# Find the latest SVN Revision and its version.
-	if $LOOKUPLATESTREVISION; then
-		# Find the Revision number.
-		CURRENTREV=`$DM $DMSOPTS http://build.chromium.org/f/chromium/snapshots/$OS/LATEST`
-		# Get the version from the SVN Revision.
-		getVersion $CURRENTREV
-		CURRENTVERSION=$RETURNEDVERSION
-	fi
+# Get information on the installed Chromium version.
+if [ -f $INSTALLPATH/$INSTALLBASE/$INSTALLNAME ]; then
+# Find version.
+INSTALLEDVERSION=`$INSTALLPATH/$INSTALLBASE/$INSTALLNAME --version | sed "s/Chromium//" | sed "s/ //g"`
+# Find SVN Revision. (Only possible on OS X sadly.)
+if [ $OS == "Mac" ]; then
+INSTALLEDREV=`cat /Applications/Chromium.app/Contents/Info.plist | grep -A 1 SVNRevision | grep -o "[[:digit:]]\+"`
+fi
+# Set installed variable for other functions.
+INSTALLED=true
+else
+INSTALLED=false
+fi
 
-	echo "Done."
+# Find the latest SVN Revision and its version.
+if $LOOKUPLATESTREVISION; then
+# Find the Revision number.
+CURRENTREV=`$DM $DMSOPTS http://build.chromium.org/f/chromium/snapshots/$OS/LATEST`
+# Get the version from the SVN Revision.
+getVersion $CURRENTREV
+CURRENTVERSION=$RETURNEDVERSION
+fi
+
+echo "Done."
 }
 # Function to install Chromium.
 #
@@ -117,116 +117,112 @@ get_info() {
 #
 install() {
 
-	# Check for a Revision number.
-	if [ -z "$1" ]; then
-		echo "Install function requires a revision variable."
-		exit
-	else
-		# Find the version.
-		getVersion $1
-		VERSION=$RETURNEDVERSION
-	fi
+# Check for a Revision number.
+if [ -z "$1" ]; then
+echo "Install function requires a revision variable."
+exit
+else
+# Find the version.
+getVersion $1
+VERSION=$RETURNEDVERSION
+fi
 
-	# Check that the version about to be installed is not the same
-	# as the version currently installed.
-	if [ "$VERSION" == "$INSTALLEDVERSION" ]; then
-		echo "The requested version is already installed. Nothing to do here."
-		exit
-	fi
+# Check that the version about to be installed is not the same
+# as the version currently installed.
+if [ "$VERSION" == "$INSTALLEDVERSION" ]; then
+echo "The requested version is already installed. Nothing to do here."
+exit
+fi
 
-	# Check for NOINSTALL.
-	if [ -z "$2" ]; then
-		NOINSTALL=false
-	else
-		NOINSTALL=true
-	fi
+# Check for NOINSTALL.
+if [ -z "$2" ]; then
+NOINSTALL=false
+else
+NOINSTALL=true
+fi
 
-	# Make a temporary folder in which to put downloaded files.
-	TMPNAME="chromium_r$1"
-	if [ ! -d $TMPNAME ]; then mkdir $TMPNAME; fi
-	cd $TMPNAME
-	
-	# Check for an existing zip.
-	if [ -d chrome-mac -o -d chrome-linux ]; then
-		read -p "Found an existing download. Do you want to use it? (Y/N) " USEEXISTING
-		case $USEEXISTING in
-			y|Y|Yes|YES)
-				USEEXISTING=true
-			;;
-			n|N|No|NO|\?)
-				USEEXISTING=false
-				rm -rf chrome-mac.zip chrome-linux.zip chrome-mac chrome-linux 2> /dev/null
-			;;
-		esac
-	else
-		USEEXISTING=false
-	fi
+# Make a temporary folder in which to put downloaded files.
+TMPNAME="chromium_r$1"
+if [ ! -d $TMPNAME ]; then mkdir $TMPNAME; fi
+cd $TMPNAME
 
-	# Download Chromium when we're not using existing file.
-	if ! $USEEXISTING ; then
+# Check for an existing zip.
+if [ -d chrome-mac -o -d chrome-linux ]; then
+read -p "Found an existing download. Do you want to use it? (Y/N) " USEEXISTING
+case $USEEXISTING in
+y|Y|Yes|YES)
+USEEXISTING=true
+;;
+n|N|No|NO|\?)
+USEEXISTING=false
+rm -rf chrome-mac.zip chrome-linux.zip chrome-mac chrome-linux 2> /dev/null
+;;
+esac
+else
+USEEXISTING=false
+fi
 
-		# ^C Trap.
-		trap "rm -rf $PWD;echo ""; exit" SIGINT
+# Download Chromium when we're not using existing file.
+if ! $USEEXISTING ; then
 
-		# Test that the revision exists.
-		REVISIONEXISTS=`$DM $DMSOPTS "http://build.chromium.org/f/chromium/snapshots/$OS/$1/REVISIONS" | grep 404`
+# ^C Trap.
+trap "rm -rf $PWD;echo ""; exit" SIGINT
 
-		if [ -z "$REVISIONEXISTS" ]; then
-			echo "Downloading r$1...			"
-			rm chrome-linux.zip chrome-mac.zip 2> /dev/null # In case there is a .n naming conflict.
-			if [ $INSTALL64BIT == true ]; then
-				$DM "http://build.chromium.org/f/chromium/snapshots/$OS_x64/$1/$ZIPNAME.zip" $DMDDOPTS
-			else
-				$DM "http://build.chromium.org/f/chromium/snapshots/$OS/$1/$ZIPNAME.zip" $DMDDOPTS
-			fi
+# Test that the revision exists.
+REVISIONEXISTS=`$DM $DMSOPTS "http://build.chromium.org/f/chromium/snapshots/$OS/$1/REVISIONS" | grep 404`
 
-		else
-			echo "Revision $1 does not exist. Fatal."
-			exit
-		fi
-		
-		# Extract.
-		printf "Extracting...\t\t\t"
-		unzip -q $ZIPNAME.zip
-		echo "Done."
+if [ -z "$REVISIONEXISTS" ]; then
+echo "Downloading r$1...			"
+rm chrome-linux.zip chrome-mac.zip 2> /dev/null # In case there is a .n naming conflict.
+$DM "http://build.chromium.org/f/chromium/snapshots/$OS/$1/$ZIPNAME.zip" $DMDDOPTS
 
-	fi
+else
+echo "Revision $1 does not exist. Fatal."
+exit
+fi
 
-	# Install
-	if ! $NOINSTALL; then
+# Extract.
+printf "Extracting...\t\t\t"
+unzip -q $ZIPNAME.zip
+echo "Done."
 
-		# If Chromium is installed...
-		if $INSTALLED; then
+fi
 
-			# Kill any running instance.
-			killall chromium 2> /dev/null
-			
-			# Delete the current version.
-			rm -rf $INSTALLPATH/$INSTALLBASE
-		fi
+# Install
+if ! $NOINSTALL; then
 
-		# If installing on Linux...
-		if [ $OS == "Linux" ]; then
-			
-			echo "Installing requires root. Please enter your password:"
-			
-			# Delete existing Chromium if it exists.
-			if $INSTALLED; then sudo rm -rf $INSTALLPATH/chromium; fi
+# If Chromium is installed...
+if $INSTALLED; then
 
-			# Copy the new Chromium.
-			sudo cp -R chrome-linux $INSTALLPATH/chromium
+# Kill any running instance.
+killall chromium 2> /dev/null
 
-			# Change ownership.
-			sudo chown -R `whoami` $INSTALLPATH/chromium
+# Delete the current version.
+rm -rf $INSTALLPATH/$INSTALLBASE
+fi
 
-			# Change Mode.
-			chmod -R 700 $INSTALLPATH/chromium/
+# If installing on Linux...
+if [ $OS == "Linux" ]; then
 
-            # If there is no Chromium entry, then make one.
-            if [ ! -f /usr/share/applications/google-chromium.desktop ]; then
+echo "Installing requires root. Please enter your password:"
 
-			# Create a .desktop for Chromium.
-            cat > google-chromium.desktop << "EOF"
+# Delete existing Chromium if it exists.
+if $INSTALLED; then sudo rm -rf $INSTALLPATH/chromium; fi
+
+# Copy the new Chromium.
+sudo cp -R chrome-linux $INSTALLPATH/chromium
+
+# Change ownership.
+sudo chown -R `whoami` $INSTALLPATH/chromium
+
+# Change Mode.
+chmod -R 700 $INSTALLPATH/chromium/
+
+# If there is no Chromium entry, then make one.
+if [ ! -f /usr/share/applications/google-chromium.desktop ]; then
+
+# Create a .desktop for Chromium.
+cat > google-chromium.desktop << "EOF"
 [Desktop Entry]
 Version=1.0
 Name=Chromium
@@ -251,98 +247,74 @@ Exec=/opt/chromium/chrome --incognito
 TargetEnvironment=Unity
 EOF
 
-            fi
+fi
 
-            # Install google-chromium.desktop
-            sudo xdg-desktop-menu install google-chromium.desktop
+# Install google-chromium.desktop
+sudo xdg-desktop-menu install google-chromium.desktop
 
-		# If Installing on OS X...
-		elif [ $OS == "Mac" ]; then
+# If Installing on OS X...
+elif [ $OS == "Mac" ]; then
 
-			# Copy the app to /Applications.
-			cp -R chrome-mac/Chromium.app /Applications/
-		fi
+# Copy the app to /Applications.
+cp -R chrome-mac/Chromium.app /Applications/
+fi
 
-		echo "Installed Chromium version $CURRENTVERSION. (SVN r$1)"
+echo "Installed Chromium version $CURRENTVERSION. (SVN r$1)"
 
-        # Cleanup.
-        rm -rf $PWD
+# Cleanup.
+rm -rf $PWD
 
-	fi
+fi
 
-	# Exit. (Prevents printing of usage.)
-	exit
+# Exit. (Prevents printing of usage.)
+exit
 
 }
 
 # Usage.
 usage(){
 
-	printf "Usage:\n"
-	printf "$0 <parameters> <options>\n"
-	printf "Note: Parameters used after options will be ignored.\n\n"
+printf "Usage:\n"
+printf -- "-u\t\t\tUpgrade Chromium to the latest SVN revision.\n"
+printf -- "-r <revision>\t\t\tInstall a specific SVN revision.\n"
+printf -- "-U\t\t\tPrint this usage.\n"
+printf -- "-v\t\t\tPrint script version.\n"
 
-	# Options.
-	printf -- "-u\t\t\tUpgrade Chromium to the latest SVN revision.\n"
-	printf -- "-r <revision>\t\t\tInstall a specific SVN revision.\n"
-	printf -- "-U\t\t\tPrint this usage.\n"
-	printf -- "-v\t\t\tPrint script version.\n\n"
-
-	# Parameters.
-	printf "Parameters:\n"
-	printf -- "-x\t\t\tUse 64-bit Chromium. (Linux only.)\n"
+exit
 
 }
 
 # Options.
-while getopts ":ur:Uvx" opt; do
-	case $opt in
-		U|\?)
-			usage
-		;;
-		u)
-			# Upgrade.
-			get_info
-			install $CURRENTREV
-		;;
-		r)
-			# Install inputted revision.
-			if [ -z "$OPTARG" ]; then
-				echo "-r requires a revision number."
-				exit
-			fi
+while getopts ":ur:Uv" opt; do
+case $opt in
+U)
+usage
+;;
+u)
+# Upgrade.
+echo "Upgrading..."
+get_info
+install $CURRENTREV
+;;
+r)
+# Install inputted revision.
+if [ -z "$OPTARG" ]; then
+echo "-r requires a revision number."
+exit
+fi
 
-			# Install the requested revision.
-			install $OPTARG
-		;;
-		v)
-			echo "Chromium Updater - version 0.9"
-			exit
-		;;
-<<<<<<< HEAD
-		x)
-
-			if [ -z "$SYSTEMCALLED" ]; then
-				system
-			fi
-
-			# Check for 64-bit architecture first...
-			if [ "$OS" == "Linux" -a `uname -p` == "x86_64" ]; then
-				echo "Using 64-bit Chromium..."
-				INSTALL64BIT=true
-			elif [ $OS == "Linux" ]; then
-				echo "This version of Linux is not 64-bit. Using 32-bit."
-			elif [ "$OS" == "Mac" ]; then
-				echo "There is no 64-bit version of Chromium for OS X. Using 32-bit."
-			fi
-		;;
-		\?)
-			echo "Invalid option -$OPTARG."
-			usage
-		;;
-=======
->>>>>>> parent of 88daea9... Fixed revision installing. Improved options.
-	esac
+# Install the requested revision.
+install $OPTARG
+;;
+v)
+echo "Chromium Updater - version 0.9"
+exit
+;;
+\?)
+echo "Invalid option -$OPTARG."
+usage
+;;
+esac
 done
 
 # Print the usage. (Should only occur if there are no switches or -U is passed.)
